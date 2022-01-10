9a1ad773ecf255fcd488642bb061c75aa6f89827
JenniferSilva48 <jennifer.silva@academico.ifpb.edu.br>
2022-01-10 18:56:34 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,28 +1,37 @@
-import {ReactComponent as Arrow} from "assets/img/nav_arrow.svg";
 
+import MovieCard from "componets/MovieCard";
+import Pagination from "componets/Pagination";
 
 import './style.css';
 
 function Listing() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+
     return (
-        <div className="dsmovie-pagination-container">
-        <div className="dsmovie-pagination-box">
-            <button className="dsmovie-pagination-button" disabled={true} >
-                <Arrow />
-            </button>
-            <p>{`${1} de ${3}`}</p>
-            <button className="dsmovie-pagination-button" disabled={false} >
-                <Arrow className="dsmovie-flip-horizontal" />
-            </button>
+        <>
+        <Pagination />
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-6">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-6">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-6">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-6">
+                    <MovieCard />
+                </div>
+            </div>
+        
         </div>
-    </div>
+       
+        </>
+        
     )
 }
 
