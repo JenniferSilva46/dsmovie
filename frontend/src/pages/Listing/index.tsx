2dace7b93242f8a83afc0ad21286afe6969a8ade
JenniferSilva48 <jennifer.silva@academico.ifpb.edu.br>
2022-01-10 17:57:20 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,28 @@
+import {ReactComponent as Arrow} from "assets/img/nav_arrow.svg";
+
+
+import './style.css';
+
 function Listing() {
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
     return (
-        <h1>Hello Listing</h1>
+        <div className="dsmovie-pagination-container">
+        <div className="dsmovie-pagination-box">
+            <button className="dsmovie-pagination-button" disabled={true} >
+                <Arrow />
+            </button>
+            <p>{`${1} de ${3}`}</p>
+            <button className="dsmovie-pagination-button" disabled={false} >
+                <Arrow className="dsmovie-flip-horizontal" />
+            </button>
+        </div>
+    </div>
     )
 }
 
